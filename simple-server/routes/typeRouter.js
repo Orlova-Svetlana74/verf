6d7a5801a7c11462d6ed080c6781fb4e5ef44969
Svetlana Orlova <sorlova#mail.ru>
2024-12-12 01:15:22 +0300

    Updates

--- simple-server/routes/typeRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const typeController = require('../controllers/typeController');
-const checkRole = require('../middleware/checkRoleMiddleware');
-
-router.post('/', checkRole ('ADMIN'), typeController.create); //создавать бренды
-router.get('/', typeController.getAll); //получать бренды
-
-module.exports = router;
